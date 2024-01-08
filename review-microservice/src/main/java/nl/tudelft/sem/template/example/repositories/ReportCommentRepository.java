adb935615b69d05860c1acada57ce5052b039a02
Egemen Yildiz <E.yildiz-2@student.tudelft.nl>
2024-01-08 17:41:42 +0100

    Adding-the-report-repository-queries

--- review-microservice/src/main/java/nl/tudelft/sem/template/example/repositories/ReportCommentRepository.java
+++ review-microservice/src/main/java/nl/tudelft/sem/template/example/repositories/ReportCommentRepository.java
@@ -2,12 +2,16 @@ package nl.tudelft.sem.template.example.repositories;
 
 import nl.tudelft.sem.template.model.ReportComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ReportCommentRepository extends JpaRepository<ReportComment, Long> {
+    @Query("SELECT rc FROM ReportComment rc WHERE rc.comment.id = :commentId")
     List<ReportComment> findAllByCommentId(Long commentId);
+
+    @Query("SELECT CASE WHEN COUNT(rc) > 0 THEN true ELSE false END FROM ReportComment rc WHERE rc.comment.id = :commentId")
     boolean existsByCommentId(Long commentId);
 }
