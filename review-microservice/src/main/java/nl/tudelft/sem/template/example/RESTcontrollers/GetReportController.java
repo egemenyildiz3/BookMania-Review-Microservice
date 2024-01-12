6b2f52ffe0a02604cc4bfeff64aeebf01f9cfaf6
Jacek Kulik <J.Kulik@student.tudelft.nl>
2024-01-12 19:20:47 +0100

    Fixed getReportController wanting some beans

--- review-microservice/src/main/java/nl/tudelft/sem/template/example/RESTcontrollers/GetReportController.java
+++ review-microservice/src/main/java/nl/tudelft/sem/template/example/RESTcontrollers/GetReportController.java
@@ -2,6 +2,7 @@ package nl.tudelft.sem.template.example.RESTcontrollers;
 
 import nl.tudelft.sem.template.api.GetReportApi;
 import nl.tudelft.sem.template.example.repositories.BookDataRepository;
+import nl.tudelft.sem.template.example.repositories.CommentRepository;
 import nl.tudelft.sem.template.example.repositories.ReviewRepository;
 import nl.tudelft.sem.template.example.services.*;
 import nl.tudelft.sem.template.model.BookData;
@@ -18,7 +19,8 @@ public class GetReportController implements GetReportApi {
 
     private final GetReportServiceImpl service;
 
-    public GetReportController(BookDataRepository repo, ReviewRepository rr, CommunicationServiceImpl cs, CommentService co) {
+    public GetReportController(BookDataRepository repo, ReviewRepository rr, CommunicationServiceImpl cs, CommentRepository cr) {
+        CommentService co = new CommentServiceImpl(cr, rr);
         this.service = new GetReportServiceImpl(repo, rr, cs, co);
     }
 
