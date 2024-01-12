0e7ef30fa626aa985cdf2d0f713a810ca3663618
Jacek Kulik <j.kulik@student.tudelft.nl>
2024-01-12 18:38:36 +0100

    Revert changes to CommentRepository as they were unintended

--- review-microservice/src/main/java/nl/tudelft/sem/template/example/repositories/CommentRepository.java
+++ review-microservice/src/main/java/nl/tudelft/sem/template/example/repositories/CommentRepository.java
@@ -1,14 +1,10 @@
 package nl.tudelft.sem.template.example.repositories;
 
 import nl.tudelft.sem.template.model.Comment;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
 }
