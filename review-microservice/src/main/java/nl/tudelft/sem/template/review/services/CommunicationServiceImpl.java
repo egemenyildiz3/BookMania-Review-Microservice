19e965a56c1b8e942d442e79d540bfcdf4864e3c
bmarinov <b.k.marinov-1@student.tudelft.nl>
2024-01-13 20:59:21 +0100

    http client integration attempt

--- review-microservice/src/main/java/nl/tudelft/sem/template/review/services/CommunicationServiceImpl.java
+++ review-microservice/src/main/java/nl/tudelft/sem/template/review/services/CommunicationServiceImpl.java
@@ -2,6 +2,11 @@ package nl.tudelft.sem.template.review.services;
 
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 @Service
 public class CommunicationServiceImpl {
     private final String userMicroUrl = "http://localhost:8080";
@@ -14,12 +19,68 @@ public class CommunicationServiceImpl {
 
     public boolean isAdmin(Long userId) {
         //TODO make http request to endpoint for admin
-        return true;
+        try {
+            String url = userMicroUrl + "/check/role/1";
+            URL obj = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+
+            // Set the request method
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                // Print the response
+                System.out.println(response.toString());
+            } else {
+                System.out.println("Failed with status code " + responseCode);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
     }
+
 
     public boolean existsBook(Long bookId) {
         //TODO make http request to endpoint for book
-        return true;
+        try {
+            String url = bookMicroUrl + "/book/getById/1";
+            URL obj = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+
+            // Set the request method
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                // Print the response
+                System.out.println(response.toString());
+            } else {
+                System.out.println("Failed with status code " + responseCode);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+        //return true;
     }
 
     public boolean existsUser(Long userId) {
